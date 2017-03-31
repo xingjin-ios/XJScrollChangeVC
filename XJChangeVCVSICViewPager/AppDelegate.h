678ce46482221d05350cf384a408be80995d7673
邢进 <xingjin@xingjindeMacBook-Pro.local>
2017-03-31 10:39:05 +0800

    Initial Commit

---
+++ XJChangeVCVSICViewPager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XJChangeVCVSICViewPager
+//
+//  Created by 邢进 on 2017/3/31.
+//  Copyright © 2017年 邢进. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XJChangeVCVSICViewPager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XJChangeVCVSICViewPager
+//
+//  Created by 邢进 on 2017/3/31.
+//  Copyright © 2017年 邢进. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
